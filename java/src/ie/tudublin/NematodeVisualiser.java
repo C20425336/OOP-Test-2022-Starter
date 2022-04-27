7fcc114437b7946d4e22b7f2c990b6cb914d085a
Mizan <C20425336@mytudublin.ie>
2022-04-27 18:54:31 +0100

    final piece

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -6,15 +6,35 @@ import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+
 public class NematodeVisualiser extends PApplet
 {
 
 	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
+	public float border;
+
+	Minim minim;
+    AudioPlayer ap;
+    AudioInput ai;
+    AudioBuffer ab;
+
 	public void keyPressed()
 	{		
 		if (keyCode == LEFT)
 		{
-		}		
+
+		}
+		
+		if (ap.isPlaying()) {
+			ap.pause();
+		} else {
+			ap.rewind();
+			ap.play();
+		}
 	}
 
 
@@ -27,22 +47,37 @@ public class NematodeVisualiser extends PApplet
 	{
 		colorMode(HSB);
 		background(0);
-		smooth();				
+		smooth();	
+		loadNematodes();
+		minim = new Minim(this);
+		ap = minim.loadFile("background.wav", 800);
+		ap.play();
+        ab = ap.mix;			
 	}
 	
 
 	public void loadNematodes()
 	{
-		Table table = loadTable("nametodes.csv", "header");
-        for(TableRow r:table.rows())
+		Table table = loadTable("nematodes.csv", "header");
+		for(TableRow r:table.rows())
         {
             Nematode n = new Nematode(r);
             nematodes.add(n);
         }
 	}
 
+	public void drawNematodes(){
+		for(Nematode n:nematodes)
+		{
+			n.render(this);
+		}
+	}
 
 	public void draw()
 	{	
+		background(0);
+		noFill();
+		stroke(255);
+		drawNematodes();
 	}
 }
